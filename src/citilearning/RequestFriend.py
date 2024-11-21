2e8f76d79d47016be2009dfff779687d54f3e3ad
yutongwang <598402463@qq.com>
2024-11-21 16:31:59 +0800

    Leetcode 825 practice

--- src/citilearning/RequestFriend.py
+++ src/citilearning/RequestFriend.py
@@ -6,23 +6,21 @@ class Solution:
         initial_count = 0
         size = 121
         count_list = [initial_count] * size
+        prefix_sum_list = [initial_count] * size
 
         for age in ages:
             count_list[age] += 1
 
+        for i in range(1, size):
+            prefix_sum_list[i] = prefix_sum_list[i - 1] + count_list[i]
+
         res = 0
         for i in range(len(count_list)):
             if count_list[i] > 0:
-                max_age = i
-                min_age = math.floor(0.5 * i + 7) + 1
-                for age in range(min_age, max_age + 1):
-                    if count_list[age] > 0:
-                        if age != i:
-                            res += count_list[age] * count_list[i]
-                        else:
-                            res += count_list[age] * (count_list[i] - 1)
+                min_age = math.floor(0.5 * i + 7)
+                res += max(count_list[i] * (prefix_sum_list[i] - prefix_sum_list[min_age] - 1), 0)
         return res
 
 if __name__ == '__main__':
     s = Solution()
-    print(s.numFriendRequests([16,17,18]))
+    print(s.numFriendRequests([108,115,5,24,82]))
